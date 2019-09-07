1944d89a1a5ad808f79bb098be8416d8a1164460
mabdc <747455334@qq.com>
2019-09-07 22:38:35 +0800

    fix

--- lib/pages/color_lens_page.dart
+++ lib/pages/color_lens_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../global/profile_change_notifier.dart';
+import '../global/global.dart';
 
 class ColorLensPage extends StatefulWidget {
   @override
@@ -16,16 +17,28 @@ class _ColorLensPageState extends State<ColorLensPage> {
   @override
   void initState() {
     super.initState();
-    
+    final customColor = Color(Global.profile.customColor);
+    r = TextEditingController(text: customColor.red.toString());
+    g = TextEditingController(text: customColor.green.toString());
+    b = TextEditingController(text: customColor.blue.toString());
+  }
+
+  void Function(String s) onChange(TextEditingController controller) {
+    return (String s) {
+      int number = int.tryParse(s);
+      if (number == null || number < 0) {
+        controller.text = "0";
+      }
+      if (number > 255) {
+         controller.text = "255";
+      }
+      setState(() {});
+    };
   }
 
   @override
   Widget build(BuildContext context) {
     final theme = Provider.of<ThemeModel>(context);
-    Color _customColor = Color(theme.color);
-    r = TextEditingController(text: _customColor.red.toString());
-    g = TextEditingController(text: _customColor.green.toString());
-    b = TextEditingController(text: _customColor.blue.toString());
     return Scaffold(
       appBar: AppBar(
         title: Text('Color Lens'),
@@ -46,13 +59,15 @@ class _ColorLensPageState extends State<ColorLensPage> {
               );
               break;
             case 1:
-              Color _customColorValue = Color(theme.customColor);
+              Color _customColor = Color.fromARGB(0xff, int.parse(r.text),
+                  int.parse(g.text), int.parse(b.text));
               return Card(
                 child: ListTile(
-                  // color: _customColorValue,
                   leading: Container(
                     decoration: BoxDecoration(
-                        shape: BoxShape.circle, color: _customColorValue),
+                      shape: BoxShape.circle,
+                      color: _customColor,
+                    ),
                     height: 32,
                     width: 32,
                   ),
@@ -61,120 +76,54 @@ class _ColorLensPageState extends State<ColorLensPage> {
                           children: <Widget>[
                             Container(
                               width: 80,
-                              padding: EdgeInsets.only(left: 20),
+                              padding: EdgeInsets.only(right: 20),
                               child: TextField(
                                 controller: r,
                                 keyboardType: TextInputType.number,
                                 decoration: InputDecoration(helperText: 'red'),
-                                maxLength: 3,
-                                onChanged: (text) {
-                                  int _r = int.parse(r.text);
-                                  int _g = int.parse(g.text);
-                                  int _b = int.parse(b.text);
-                                  setState(() {
-                                    theme.customColor = Color.fromARGB(
-                                      0xff,
-                                      _r > 0xff ? 0xff : _r,
-                                      _g > 0xff ? 0xff : _g,
-                                      _b > 0xff ? 0xff : _b,
-                                    ).value;
-                                  });
-                                },
-                                onSubmitted: (text) {
-                                  _enEdit = false;
-                                  int _r = int.parse(r.text);
-                                  int _g = int.parse(g.text);
-                                  int _b = int.parse(b.text);
-                                  theme.customColor = Color.fromARGB(
-                                    0xff,
-                                    _r > 0xff ? 0xff : _r,
-                                    _g > 0xff ? 0xff : _g,
-                                    _b > 0xff ? 0xff : _b,
-                                  ).value;
-                                },
+                                onChanged: onChange(r),
                               ),
                             ),
                             Container(
                               width: 80,
-                              padding: EdgeInsets.only(left: 20),
+                              padding: EdgeInsets.only(right: 20),
                               child: TextField(
                                 controller: g,
                                 keyboardType: TextInputType.number,
                                 decoration:
                                     InputDecoration(helperText: 'green'),
-                                maxLength: 3,
-                                onChanged: (text) {
-                                  int _r = int.parse(r.text);
-                                  int _g = int.parse(g.text);
-                                  int _b = int.parse(b.text);
-                                  theme.customColor = Color.fromARGB(
-                                    0xff,
-                                    _r > 0xff ? 0xff : _r,
-                                    _g > 0xff ? 0xff : _g,
-                                    _b > 0xff ? 0xff : _b,
-                                  ).value;
-                                },
-                                onSubmitted: (text) {
-                                  _enEdit = false;
-                                  int _r = int.parse(r.text);
-                                  int _g = int.parse(g.text);
-                                  int _b = int.parse(b.text);
-                                  theme.customColor = Color.fromARGB(
-                                    0xff,
-                                    _r > 0xff ? 0xff : _r,
-                                    _g > 0xff ? 0xff : _g,
-                                    _b > 0xff ? 0xff : _b,
-                                  ).value;
-                                },
+                                onChanged: onChange(g),
                               ),
                             ),
                             Container(
                               width: 80,
-                              padding: EdgeInsets.only(left: 20),
+                              padding: EdgeInsets.only(right: 20),
                               child: TextField(
                                 controller: b,
                                 keyboardType: TextInputType.number,
                                 decoration: InputDecoration(helperText: 'blue'),
-                                maxLength: 3,
-                                onChanged: (text) {
-                                  int _r = int.parse(r.text);
-                                  int _g = int.parse(g.text);
-                                  int _b = int.parse(b.text);
-                                  theme.customColor = Color.fromARGB(
-                                    0xff,
-                                    _r > 0xff ? 0xff : _r,
-                                    _g > 0xff ? 0xff : _g,
-                                    _b > 0xff ? 0xff : _b,
-                                  ).value;
-                                },
-                                onSubmitted: (text) async {
-                                  _enEdit = false;
-                                  int _r = int.parse(r.text);
-                                  int _g = int.parse(g.text);
-                                  int _b = int.parse(b.text);
-                                  theme.customColor = Color.fromARGB(
-                                    0xff,
-                                    _r > 0xff ? 0xff : _r,
-                                    _g > 0xff ? 0xff : _g,
-                                    _b > 0xff ? 0xff : _b,
-                                  ).value;
-                                },
+                                onChanged: onChange(b),
                               ),
                             ),
                           ],
                         )
                       : Text(
-                          'custom color  r:${_customColorValue.red} g:${_customColorValue.green} b:${_customColorValue.blue}'),
-                  subtitle: _enEdit
-                      ? Container()
-                      : Text('tap to active & long press to edit'),
+                          'custom color  r:${_customColor.red} g:${_customColor.green} b:${_customColor.blue}'),
+                  trailing: IconButton(
+                    icon: Icon(_enEdit ? Icons.save : Icons.mode_edit),
+                    onPressed: () {
+                      if (_enEdit) {
+                        _enEdit = false;
+                        setState(() {});
+                        theme.customColor = _customColor.value;
+                      } else {
+                        _enEdit = true;
+                        setState(() {});
+                      }
+                    },
+                  ),
                   onTap: () async {
                     theme.color = theme.customColor;
-                  },
-                  onLongPress: () async {
-                    setState(() {
-                      _enEdit = true;
-                    });
                   },
                 ),
               );
